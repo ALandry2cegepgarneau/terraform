b543d02fbed6008c8229b9983571ddbd7ae31b79
Arnaud Landry <alandry@egyde.ca>
2022-05-04 21:49:01 -0400

    add main

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    },
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
-    }
-  }
-}
 provider "azurerm" {
   features {}
 }
