9af441b1dca63c4d5e4b913cb6fc6d0677b4b485
Arnaud Landry <alandry@egyde.ca>
2022-05-10 12:12:23 -0400

    update workflow

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -12,7 +12,7 @@ terraform {
 variable "location" {
   type        = string
   description = "Location of Azure resources"
-  default     = "eastus"
+  default     = "canadaeast"
 }
 variable "resource_group_name" {
   type        = string
@@ -33,24 +33,24 @@ resource "azurerm_resource_group" "demo" {
   name     = var.resource_group_name
   location = var.location
 }
-resource "azurerm_container_group" "aci" {
-  name                = var.container_group_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  ip_address_type     = "Public"
-  dns_name_label      = var.container_group_dns
-  os_type             = "Linux"
-  container {
-    name   = "nginx"
-    image  = "nginx:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
-  }
-  depends_on = [
-    azurerm_resource_group.demo,
-  ]
-}
+#resource "azurerm_container_group" "aci" {
+#  name                = var.container_group_name
+#  resource_group_name = var.resource_group_name
+#  location            = var.location
+#  ip_address_type     = "Public"
+#  dns_name_label      = var.container_group_dns
+#  os_type             = "Linux"
+#  container {
+#    name   = "nginx"
+#    image  = "nginx:latest"
+#    cpu    = "0.5"
+#    memory = "1.5"
+#    ports {
+#      port     = 80
+#      protocol = "TCP"
+#    }
+#  }
+#  depends_on = [
+#    azurerm_resource_group.demo,
+#  ]
+#}
